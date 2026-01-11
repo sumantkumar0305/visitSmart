bbc057c0ce4dc1a10270560017d9ced5bc6dc59a
Sumant Kumar <sumant.kumar0305@gmail.com>
2026-01-11 18:06:57 +0530

    Refactor HotelImage component for better readability

--- frontend/src/AllCode/MainBodyCode/AboutCardCode/HotelCode/HotelCard/HoteLImage.jsx
+++ frontend/src/AllCode/MainBodyCode/AboutCardCode/HotelCode/HotelCard/HoteLImage.jsx
@@ -1,45 +1,48 @@
-import { Box } from "@mui/material";
-import { PhotoCamera } from "@mui/icons-material";
+import { Box } from "@mui/material"
+import { PhotoCamera } from "@mui/icons-material"
 
-export default function HotelImage({ hotel }) {
-  return (
-    <Box
-      sx={{
-        height: 220,
-        overflow: "hidden",
-        position: "relative",
-      }}
-    >
-      {hotel?.data?.image?.length > 0 ? (
-        <Box
-          component="img"
-          src={hotel.data.image[0]}
-          alt={hotel?.data?.name || "Hotel image"}
-          sx={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            transition: "transform 0.4s ease",
-            "&:hover": {
-              transform: "scale(1.1)",
-            },
-          }}
-        />
-      ) : (
-        <Box
-          sx={{
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            bgcolor: "#f0f0f0",
-            color: "grey.400",
-          }}
-        >
-          <PhotoCamera sx={{ fontSize: 60 }} />
-        </Box>
-      )}
-    </Box>
-  );
+export default function HotelImage({hotel}){
+    const image = hotel?.image;
+    return(
+        <>
+            <Box
+                sx={{
+                height: 220,
+                overflow: "hidden",
+                position: "relative",
+                }}
+            >
+                {Array.isArray(image) && image.length > 0 ? (
+                <Box
+                    component="img"
+                    src={image[0]}
+                    alt={hotel.name}
+                    sx={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                    transition: "transform 0.4s ease",
+                    "&:hover": {
+                        transform: "scale(1.1)", // Clean zoom effect
+                    },
+                    }}
+                />
+                ) : (
+                <Box
+                    sx={{
+                    width: "100%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    bgcolor: "#f0f0f0",
+                    color: "grey.400",
+                    }}
+                >
+                    <PhotoCamera sx={{ fontSize: 60 }} />
+                </Box>
+                )}
+            </Box>
+        </>
+    )
 }
